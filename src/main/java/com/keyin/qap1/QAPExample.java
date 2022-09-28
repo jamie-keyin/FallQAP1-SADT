71ea0fa1611ba4fd7d2c46cc3c033f5d19a8c549
jamie.cornick <jamie.cornick@keyin.com>
2022-09-28 09:57:47 -0230

    Initial commit

---
+++ src/main/java/com/keyin/qap1/QAPExample.java
@@ -0,0 +1,7 @@
+package com.keyin.qap1;
+
+public class QAPExample {
+    public boolean findSequenceInWord(String wordToFind, String charSequence) {
+        return charSequence.contains(wordToFind);
+    }
+}
---
+++ src/test/java/com/keyin/qap1/QAPExampleTest.java
@@ -0,0 +1,27 @@
+package com.keyin.qap1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class QAPExampleTest {
+
+    @Test
+    public void qapExampleTest1() {
+        QAPExample qapExample = new QAPExample();
+
+        Assertions.assertTrue(qapExample.findSequenceInWord("find", "is_find_in_this_char_sequence?"));
+
+        Assertions.assertFalse(qapExample.findSequenceInWord("find_me", "is_find_in_this_char_sequence?"));
+    }
+
+    @Test
+    public void qapExampleTest2() {
+
+    }
+
+
+    @Test
+    public void qapExampleTest3() {
+
+    }
+}
